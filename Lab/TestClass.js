12f93078483cde0152d9cab45b3ff8e585cc8250
Nural <turkmen.nural2@gmail.com>
2023-01-28 13:25:48 -0500

    Homework2

---
+++ Lab/TestClass.js
@@ -0,0 +1,14 @@
+let sentence1 = "NPR news, audios, and podcast. Coverage of breaking stories, national and world news, politics, business, science, technology, and extended coverage of, View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.com.";
+
+/**
+* if the lenght of sentence1 is greater than 100 or sentence1 starts with 'go to'
+*/
+// let conditionRes = sentence1.lenght > 100
+
+// let conditionRes = sentence1 starts with ("go to");
+
+console.log(sentence1. length>100 || sentence1 ("go to"));
+
+let sentence2 = "Go to NBCNews.com for breaking news, videos, and the latest top stories in world news, business, politics, health and pop culture.";
+
+console.log(sentence2. length>100 || sentence2 ("go to") );
